da045009627bf466aca01611dadfcb70f4e61cee
NiatirbAela <Aleahbritain@gmail.com>
2025-11-30 22:27:04 -0800

    Update training models.py

--- training models.py
+++ training models.py
@@ -98,3 +98,31 @@ def get_dataloaders() -> Tuple[DataLoader, DataLoader]:
 # =================================
 # Build class for each model
 # =================================
+
+'''Fully Connected Layers:
+Input: 1x28x28=784 (flatten)
+FC1: 784 -> 256
+Nonlinear: ReLU
+FC2: 256 -> 128
+Nonlinear: ReLU
+FC3: 128 -> 10 (AKA 0-9)
+Nonlinear: ReLU
+'''
+class FC(nn.Module):
+    super().__init__()
+    self.fc1=nn.Linear(784,256)
+    self.fc2=nn.Linear(256,128)
+    self.fc3=nn.Linear(128,10)
+    self.nonlin=nn.ReLU()
+
+def forward(self, x):
+    x = self.fc1(x)
+    x = self.nonlin(x)
+    x = self.fc2(x)
+    x = self.nonlin(x)
+    x = self.fc3(x)
+    x = self.nonlin(x)
+    return x 
+
+
+
